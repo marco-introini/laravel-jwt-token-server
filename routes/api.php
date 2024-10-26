d2c175ee60c4a02205f5815912daef2c7363023a
Marco "Mint" Introini <mint.dev@pm.me>
2024-10-26 10:22:31 +0200

    Add HS256 JWT check endpoint
    Introduced a new route for checking HS256 JWT tokens by adding JwtCheckController. Also, added a test case for this new endpoint in `tests/Http/CheckJwt.http`.

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,12 @@
 <?php
 
+use App\Http\Controllers\Api\JwtCheckController;
 use App\Http\Controllers\Api\LoginController;
 use App\Http\Middleware\RequiresJsonMiddleware;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(RequiresJsonMiddleware::class)->group(function () {
     Route::get('/login', LoginController::class);
+    Route::get('/checkHs256', [JwtCheckController::class, 'checkHS256']);
 });
 
